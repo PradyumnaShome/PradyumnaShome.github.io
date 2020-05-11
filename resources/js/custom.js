418b091281a833a86ebe786ea4f536b51d5004e5
Pradyumna Shome <pradyumna.shome@gmail.com>
2020-05-11 02:40:13 -0500

    Added Stanford CS 106A teaching experience.
    - Added feature to filter by experience type.
    - The filter types are industry, research, teaching, and all (default)

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -23,3 +23,31 @@ function onCvButtonClick(event) {
     resumeContainer.scrollIntoView();
   }
 }
+
+/**
+ * Filters the experience items by type.
+ * @param {event} event The click event.
+ * @param {section} section The section the user wants to switch to.
+ */
+function filterByExperienceType(event, section) {
+  // Make currently active tab and tab link inactive
+  const openTabs = document.querySelectorAll("section.experience-item");
+
+  const ACTIVE_EXPERIENCE_CLASS = "experience-active";
+  const ACTIVE_TABITEM_CLASS = "tablink-active";
+
+  openTabs.forEach(openTab => {
+    const shouldDisplayTab = section == "all" || openTab.classList.contains("experience-"+section);
+    if (shouldDisplayTab) {
+      if (!openTab.classList.contains(ACTIVE_EXPERIENCE_CLASS)) {
+        openTab.classList.add(ACTIVE_EXPERIENCE_CLASS);
+      }
+    } else {
+      openTab.classList.remove(ACTIVE_EXPERIENCE_CLASS)
+    }
+  })
+
+  const openTabLink = document.querySelector("button.tablinks." + ACTIVE_TABITEM_CLASS);
+  openTabLink.classList.toggle(ACTIVE_TABITEM_CLASS);
+  event.currentTarget.classList.toggle(ACTIVE_TABITEM_CLASS);
+}
