f7b5196b754cc6f716cce28b11129e4e0a4f61e4
Pradyumna Shome <pradyumna.shome@gmail.com>
2020-09-03 03:43:27 -0500

    Experience information is loaded from a YAML file.
    - Added dynamic data injection and layout inflation
    - Moved all experience to YAML file
    - Updated icons

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -54,3 +54,90 @@ function filterByExperienceType(event, section) {
   event.currentTarget.classList.toggle(ACTIVE_TABITEM_CLASS);
   gtag("event", `section.${section}.view`, {});
 }
+
+window.onload = () => {
+  getExperienceList().then(list => {
+    renderExperience(list);
+  }).catch(error => {
+    console.log(error);
+  });
+}
+
+// Reads and parses experience list file
+const getExperienceList = async () => {
+    const response = await fetch("/resources/content/experience.yaml");
+    const responseText = await response.text();
+    return await jsyaml.load(responseText);
+}
+
+// Generates an individual experience item card with data
+const generateExperienceItem = (itemData) => {
+    let experienceElement = document.createElement("section");
+    experienceElement.classList.add("experience-item", "experience-active", "experience-" + itemData.type);
+    
+    let experienceImage = document.createElement("img");
+    experienceImage.setAttribute("src", itemData.icon);
+    experienceElement.appendChild(experienceImage);
+
+    // Heading
+    let experienceHeading = document.createElement("h3");
+
+    let experienceOrganization = document.createElement("span");
+    experienceOrganization.classList.add("organization");
+
+    // Heading -> Link
+    let experienceOrganizationLink = document.createElement("a");
+    experienceOrganizationLink.setAttribute("href", itemData.link);
+    
+    experienceOrganizationLink.innerHTML = itemData.organization;
+    experienceOrganization.appendChild(experienceOrganizationLink);
+    experienceHeading.appendChild(experienceOrganization);
+
+    // Heading -> Role
+    let experienceRole = document.createElement("span");
+    experienceRole.classList.add("role");
+
+    let experienceSeparator = document.createElement("span");
+    experienceSeparator.classList.add("separator");
+    experienceSeparator.innerHTML = " · ";
+
+    let experienceDuration = document.createElement("span");
+    experienceDuration.classList.add("duration");
+    experienceDuration.innerHTML = itemData.duration;
+
+    experienceRole.innerText = itemData.role;
+    experienceRole.appendChild(experienceSeparator);
+    experienceRole.appendChild(experienceDuration);
+
+    experienceHeading.appendChild(experienceRole);
+    
+    experienceElement.appendChild(experienceHeading);
+
+    // Description
+    let experienceDescription = document.createElement("article");
+    experienceDescription.classList.add("description");
+    
+    let experienceDescriptionList = document.createElement("ul");
+
+    let experienceDescriptionListItems = itemData.description.map(textItem => {
+      let bulletPoint = document.createElement("li");
+      bulletPoint.innerHTML = textItem;
+      return bulletPoint;
+    });
+    experienceDescriptionListItems.forEach(item => {
+      experienceDescriptionList.appendChild(item);
+    })
+    experienceDescription.appendChild(experienceDescriptionList);
+    experienceElement.appendChild(experienceDescription);
+
+    return experienceElement;
+}
+
+// Uses a list of experiences to render the experience section
+const renderExperience = (experienceList) => {
+    const experienceSection = document.getElementById("experience");
+
+    experienceList.forEach((item) => {
+        experienceSection.appendChild(generateExperienceItem(item));
+    });
+}
