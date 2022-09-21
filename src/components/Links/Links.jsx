4e426beb9c9339b48cc4c5b663a631da3eabe56f
Pradyumna Shome <pradyumna.shome@gmail.com>
2022-09-21 01:03:15 -0400

    Fix element nesting order and list key issues

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -4,14 +4,12 @@ import linksList from './linksList.yaml';
 export default function Links() {
     return <section className="links">
                 <h3 className="subheading">Links</h3>
-                <p>
-                    <ol>
-                    {
-                        linksList.links.map(item => {
-                            return <li><a href={item.url} target="_blank" rel="noopener noreferrre">{item.text}</a></li>
-                        })
-                    }
-                    </ol>
-                </p>
+                <ol>
+                {
+                    linksList.links.map((item, index) => {
+                        return <li key={index}><a href={item.url} target="_blank" rel="noopener noreferrre">{item.text}</a></li>
+                    })
+                }
+                </ol>
     </section>
 }
--- src/components/music/music.js
+++ src/components/music/music.js
@@ -7,11 +7,9 @@ export default function Music() {
             <h2>Music</h2>
         </header>
         <p>
-        <p>
-                I've been playing the piano for many years, have tried my hand at music production, and am always discovering new music to listen to.
-                Genres I've been exploring recently include K-pop, Mandopop, video game soundtracks, alternative rock, sea shanties, and more.
-                Enjoy listening to some of my early efforts at digital music composition and feel free to follow me on <a href="https://open.spotify.com/user/x1lo68pet3v5jq25s7r8zakyq" target="_blank">Spotify</a>.
-            </p>
+            I've been playing the piano for many years, have tried my hand at music production, and am always discovering new music to listen to.
+            Genres I've been exploring recently include K-pop, Mandopop, video game soundtracks, alternative rock, sea shanties, and more.
+            Enjoy listening to some of my early efforts at digital music composition and feel free to follow me on <a href="https://open.spotify.com/user/x1lo68pet3v5jq25s7r8zakyq" target="_blank">Spotify</a>.
         </p>
         <p id="soundcloud">
             <iframe title="Pradyumna Shome's SoundCloud" width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/143006461&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
