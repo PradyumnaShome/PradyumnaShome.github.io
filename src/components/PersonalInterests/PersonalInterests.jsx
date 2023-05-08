8cbf249d7ff0c126594abb3b3975a601ef5f133a
Pradyumna Shome <pradyumna.shome@gmail.com>
2023-05-08 03:53:32 -0400

    Render links in personal interests section

--- src/components/PersonalInterests/PersonalInterests.jsx
+++ src/components/PersonalInterests/PersonalInterests.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import personalInterestsList from './personalInterestsList.yaml';
+import { remark } from 'remark';
+import remarkHtml from 'remark-html';
+import "./PersonalInterests.css";
+
+function renderMarkdown(remarkProcessor, markdown) {
+    return remark().use(remarkHtml).processSync(markdown).toString();
+}
 
 export default function PersonalInterests() {
+    const remarkProcessor = remark().use(remarkHtml);
     if (personalInterestsList.list.length === 0) {
         return null;    
     }
+    const zerothInterest = renderMarkdown(remarkProcessor, personalInterestsList.list[0]).trim();
+    const otherInterests = personalInterestsList.list.slice(1).map((interest, index) => {
+        const content = renderMarkdown(remarkProcessor, interest).trim();
+        return <span className="personal-interest"
+                     idx={index} 
+                     dangerouslySetInnerHTML={{__html: ", " + content}}
+        />;
+    });
     return <section className="PersonalInterests">
         <h3 className="subheading">Personal interests</h3>
         <p>
-            {personalInterestsList.list.join(", ")}
+            <span className="personal-interest" dangerouslySetInnerHTML={{__html: zerothInterest}}/>
+            {otherInterests}
         </p>
     </section>
 }
