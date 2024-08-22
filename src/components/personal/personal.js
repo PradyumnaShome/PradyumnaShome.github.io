5d959ced974185de943cf03089fe539af6a05e61
Pradyumna Shome <pradyumna.shome@gmail.com>
2024-08-22 18:31:37 -0400

    Add writings

--- src/components/personal/personal.js
+++ src/components/personal/personal.js
@@ -3,9 +3,35 @@ import './personal.css';
 import PersonalInterests from '../PersonalInterests/PersonalInterests';
 
 export default function Personal() {
-    return <section id="music" className="main special">
+    return <section id="personal" className="main special">
         <header className="major">
             <h2>Personal</h2>
+            <h3>Writings</h3>
+            <ul>
+                <li>
+                    <a href="https://collective.flashbots.net/t/closing-the-intel-tdx-page-fault-side-channel-or-the-case-for-tdexit-notify/3775">
+                        Closing the Intel TDX Page Fault Side Channel, or, The Case for TDExit-Notify
+                    </a>
+                </li>
+                <li>
+                    <a href="https://collective.flashbots.net/t/intel-tdx-security-and-side-channels/3648">
+                        Intel TDX Security and Side Channels
+                    </a>
+                </li>
+                <li>
+                    <a href="https://collective.flashbots.net/t/crypto-crossroads-reflections-from-an-ic3-summer-camp-rookie/3589">
+                        Crypto Crossroads: Reflections from an IC3 Summer Camp Rookie
+                    </a>
+                </li>
+                <li>
+                    <a href="https://medium.com/@pradyumnashome/undergraduate-research-in-computer-science-lessons-growth-advice-804bdabb4a1c">
+                        Undergraduate Research in Computer Science: Lessons, Growth, Advice
+                    </a>
+                </li>
+                <li>
+                    <a href="https://medium.com/fair-bytes/adversarial-machine-learning-an-overview-1b0a078f5ee0">Adversarial Machine Learning: An Overview</a>
+                </li>
+            </ul>
         </header>
         <PersonalInterests/>
     </section>
